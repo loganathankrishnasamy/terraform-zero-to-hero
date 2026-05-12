cdfc64bb5daf66f278296f8b2f23f1ae77977e26
Logan <logurathnam@gmail.com>
2026-05-12 17:21:10 +0530

    modifed main

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "ec2_instance" {
+module "ec2_instance_logan" {
   source = "./modules/ec2_instance_logan"
   ami_value = "ami-091138d0f0d41ff90" # replace this
   instance_type_value = "t3.micro"
