c22121258ba694586c61b621a51ce22b4f727163
Logan <logurathnam@gmail.com>
2026-05-12 17:32:36 +0530

    added output.tf

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "ec2_instance_logan" {
-  source = "./modules/ec2_instance_logan"
-  ami_value = "ami-091138d0f0d41ff90" # replace this
+  source              = "./modules/ec2_instance_logan"
+  ami_value           = "ami-091138d0f0d41ff90" # replace this
   instance_type_value = "t3.micro"
- }
+}
